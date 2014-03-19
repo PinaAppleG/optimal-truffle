b3b8017a0c96e34aa2017308327dde899a9444b4
Nathaniel Meyer <nathaniel.meyer@gatech.edu>
2014-03-19 16:42:15 -0400

    Working copy

--- CS7641-02/src/edu/gatech/cs7641/assignment2/part1/TrainNNWeightsWithSA.java
+++ CS7641-02/src/edu/gatech/cs7641/assignment2/part1/TrainNNWeightsWithSA.java
@@ -2,34 +2,38 @@ package edu.gatech.cs7641.assignment2.part1;
 
 import java.util.Random;
 
+import opt.OptimizationAlgorithm;
 import opt.RandomizedHillClimbing;
+import opt.SimulatedAnnealing;
 import edu.gatech.cs7641.assignment2.util.Timer;
 
-public class TrainNNWeightsWithRHC {
+public class TrainNNWeightsWithSA {
 
 	private static final int RESTARTS = 100;
 	private static final long SEED = 1L;
-	private static final int NEIGHBORHOOD_SIZE = 10;
+	private static final int NEIGHBORHOOD_SIZE = 1000;
+	private static final double COOLING = 0.1;
+	private static final double STARTING_TEMP = Double.MAX_VALUE/2;
 
 	public static void main(String[] args) {
 		Random random = new Random(SEED);
-		RandomizedHillClimbing optimizer, bestOptimizer;
+		OptimizationAlgorithm optimizer, bestOptimizer;
 		System.out.print("0, ");
-		bestOptimizer = climbSomeHill(random);
+		bestOptimizer = simulateAnnealing(random);
 		Timer timer = new Timer();
 		timer.start();
 		for (int i = 1; i < RESTARTS; i++) {
 			System.out.print(i+", ");
-			optimizer = climbSomeHill(random);
+			optimizer = simulateAnnealing(random);
 			if(optimizer.getOptimizationProblem().value(optimizer.getOptimal())
 				>bestOptimizer.getOptimizationProblem().value(bestOptimizer.getOptimal())) bestOptimizer=optimizer;
 		}
 		timer.stop();
 	}
 
-	private static RandomizedHillClimbing climbSomeHill(Random random) {
+	private static OptimizationAlgorithm simulateAnnealing(Random random) {
 		NNWeightsHillClimbingProblem hcp = new NNWeightsHillClimbingProblem(random);
-		RandomizedHillClimbing optimizer = new RandomizedHillClimbing(hcp);
+		SimulatedAnnealing optimizer = new SimulatedAnnealing(STARTING_TEMP, COOLING, hcp);
 		double fitness = -Double.MAX_VALUE, newFitness, epsilon = 0.01;
 		int neighborsChecked = 0;
 		Timer timer = new Timer();
